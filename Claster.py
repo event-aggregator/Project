93943ce6f18f2fdd314d8363eb00bf07911e1ce8
SvetaGolovina <44226980+SvetaGolovina@users.noreply.github.com>
2019-12-15 21:28:34 +0300

    Update Claster.py
    Функция clast(user, key, num) - возвращает номер темы, совпадающей с интересами пользователя

--- Claster.py
+++ Claster.py
@@ -17,45 +17,39 @@ def preprocessing(line):
 
 stemmer = SnowballStemmer("english") 
 
-a=['C++', 'Python', 'Java', 'C', 'HTML', 'CSS', 'JavaScript', 'C#', 'Go', 'Ruby','Perl', 'Pascal', 'PHP', 'Haskell', 'Swift', 'F#', 'Erlang', 'Kotlin','Scala', 'Delphi', 'Basic', 'LISP', 'D', 'Clojure', 'R', 'MATLAB', 'Objective-C','Elm', 'Elixir', 'Dart', 'TypeScript', 'Lua', 'Prolog']
-b = ['Backend development','Frontend development','UI UX design','Machine learning','AI (Artificial Intelligence)','Data sience','Mobile app development','Game development','DevOps','Big Data','Software engineering','Information security','Deep Learning','Meet Up','Discuss Speak club','Learn forum','Web technology','cloud technology']
-
 #блок параметров, которые передаются от backend из другой программы
-num = #чисто тем
+#num - чисто тем
 
-c = #интересы пользователя из анкеты в формате list (например: c = ['CSS', 'Erlang', 'Machine learning', 'DevOps', 'Game development'])
+#user - интересы пользователя из анкеты в формате list (например: user = ['CSS', 'Erlang', 'Machine learning', 'DevOps', 'Game development'])
 
-key = #ключевые слова для каждой темы в формате list (key[i] - список ключевый слов i темы, key[i][j] - j ключевое слово в i теме)
+#key - ключевые слова для каждой темы в формате list (key[i] - список ключевый слов i темы, key[i][j] - j ключевое слово в i теме)
 #конец блока
 
+def clast(user, key,num):
 
-#кластеризация интересов и тем   
-key_pr = []
-c_pr = []
-key_pr = key.copy()
-key_pr = preprocessing(key)
-c_pr = preprocessing(c)
-c_str = ' '.join(c_pr)
-lines_for_predicting =[]
-lines_for_predicting= key_pr.copy()
-lines_for_predicting.append(c_str)
+    #кластеризация интересов и тем   
+    key_pr = []
+    user_pr = []
+    key_pr = key.copy()
+    key_pr = preprocessing(key)
+    user_pr = preprocessing(user)
+    user_str = ' '.join(user_pr)
+    lines_for_predicting =[]
+    lines_for_predicting= key_pr.copy()
+    lines_for_predicting.append(user_str)
 
-tfidf_vectorizer = TfidfVectorizer()
-tfidf = tfidf_vectorizer.fit_transform(key_pr)
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf = tfidf_vectorizer.fit_transform(key_pr)
 
-kmeans = KMeans(n_clusters=num).fit(tfidf)
+    kmeans = KMeans(n_clusters=num).fit(tfidf)
 
-v = kmeans.predict(tfidf_vectorizer.transform(lines_for_predicting))
-#print(v)
+    v = kmeans.predict(tfidf_vectorizer.transform(lines_for_predicting))
 
-i = 0
-top = 10
-while top == 10:
-    if v[i]==v[num]:
-        top = i
-    else:
-        i+=1
-#важно! данные для анализа
-#!
-#v[i] - номер подходящей темы
-#!
+    i = 0
+    top = 10
+    while top == 10:
+        if v[i]==v[num]:
+            top = i
+        else:
+            i+=1
+    return v[i]
